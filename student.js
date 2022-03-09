cbeab20022fdf314ff2b9209f5b768d56252ac06
SubhashMandal01 <100958297+SubhashMandal01@users.noreply.github.com>
2022-03-09 17:53:56 +0530

    Update student.js

--- student.js
+++ student.js
@@ -11,21 +11,27 @@ var form=document.createElement('form');
 form.id='formId'
 
 div.appendChild(form)
+
 //label And input1
 var label=document.createElement('label');
 label.textContent="Name:"
 form.appendChild(label);
 var br=document.createElement('br');
 form.appendChild(br);
+let saveindex=document.createElement('input')
+saveindex.type="hidden"
+saveindex.id='saveindex'
+form.appendChild(saveindex);
 
 var input1=document.createElement('input');
 input1.type='text';
 input1.autocomplete="off"
 input1.name='fname';
-input1.id='fname';
+input1.id='fullName';
 form.appendChild(input1);
 var br=document.createElement('br');
 form.appendChild(br);
+
 
 //label And input3
 var label3=document.createElement('label');
@@ -35,9 +41,9 @@ var br=document.createElement('br');
 form.appendChild(br);
 
 var input3=document.createElement('input');
-input3.type='text';
+input3.type='number';
 input3.name='roll';
-input3.id='roll';
+input3.id='roll_number';
 input3.autocomplete="off"
 form.appendChild(input3);
 var br=document.createElement('br');
@@ -62,11 +68,23 @@ var br1=document.createElement('br');
 form.appendChild(br1);
 
 // creating button
-var button=document.createElement('button')
+let button=document.createElement('button')
 button.className='btn1'
 button.id='btn';
-button.textContent="Save";
+button.textContent="Submit";
 form.appendChild(button);
+
+// let button1=document.createElement('button')
+// button1.className='button1'
+// button1.id='button1';
+// button1.textContent="Save";
+// form.appendChild(button1);
+
+let button121=document.createElement('button')
+button121.className='button1'
+button121.id='button1';
+button121.textContent="Clear";
+form.appendChild(button121);
 // Creation of Show and delete table
 var div2=document.createElement('div')
 div2.id="contain_div";
@@ -85,156 +103,152 @@ var trow= document.createElement('tr')
 trow.id="tableRow"
 thead.appendChild(trow)
 let th2=document.createElement('th')
-th2.id="theading"
+th2.id="theading1"
 th2.textContent="Name"
 trow.appendChild(th2)
 let th3=document.createElement('th')
 trow.appendChild(th3)
-th3.id="theading"
+th3.id="theading2"
 th3.textContent="Roll Number"
 let th4=document.createElement('th')
 trow.appendChild(th4)
-th4.id="theading"
+th4.id="theading3"
 th4.textContent="Mobile Number"
 let th5=document.createElement('th')
 trow.appendChild(th5)
-th5.id="theading"
+th5.id="theading4"
+
 var tbody=document.createElement('tbody')
 tbody.id="tabody"
 tb.appendChild(tbody)
-// perform task on Show and delete table 
 
-// onclick 
-button.setAttribute("onClick", "SaveData")
+/*performing task on localstorage(display the data and edit, delete the data)
+when click save button the data will be save on localstorage as well display on browser*/
+button.setAttribute("onClick", "SaveData");
  document.addEventListener("DOMContentLoaded", () => 
  	{
       document.getElementById("btn").addEventListener("click", SaveData);
   }) ;
 
  let stdArray = [];
- var selectedRow = null
+ var selectedindex= -1;
  
     const SaveData = (e) => 
     {
       e.preventDefault();
-     
-        let stdObj = {
-          fullName: document.getElementById("fname").value,
-          roll_number: document.getElementById("roll").value,
+      let stdObj = {
+          fullName: document.getElementById("fullName").value,
+          roll_number: document.getElementById("roll_number").value,
           mobile: document.getElementById("mobile").value,
-        
-        };
+         };
+         if(selectedindex === -1)
+         {
 
-       stdArray.push(stdObj);  
-        const myJson = JSON.stringify(stdArray);
-        localStorage.setItem("StudentJson", myJson);
-        const text = localStorage.getItem("StudentJson");
-        obj = JSON.parse(text);
-
-       var formData = readFormData();
-        if (selectedRow == null)
-        {
-            insertNewRecord(formData);
-
-        }
-        else
-        {
-            updateRecord(formData);
-            
-        }       
-        resetForm();
-        
-
+           stdArray.push(stdObj); 
+         }
+         else{
+           stdArray.splice(selectedindex, 1, stdObj);
+         }
+         localStorage.StudentJson = JSON.stringify(stdArray);
+       init();
+       clear();
 }
 
-//Insert the data 
-function insertNewRecord(data)
+// when refreshing the browser the data wont be cleared with the help of this function.
+function init(){
+  var tabody=document.getElementById('tabody').textContent="";
+  if(localStorage.StudentJson)
+  {
+    stdArray=JSON.parse(localStorage.StudentJson);
+    for(var i = 0; i < stdArray.length; i++)
+    {
+       // console.log(i,stdArray[i].fullName,stdArray[i].roll_number,stdArray[i].mobile)
+      insertNewRecord(i,stdArray[i].fullName,stdArray[i].roll_number,stdArray[i].mobile);
+
+    }
+  }
+}
+// .........................................
+
+//this function comes up from the save button to display the data on browser. 
+function insertNewRecord(index,fullName,roll_number,mobile)
 {
-   var table = document.getElementById("tabody");
+   
+    var table = document.getElementById("tabody");
     let newRow = table.insertRow();
     let cell1 = newRow.insertCell(0);
     let cell2 = newRow.insertCell(1);
     let cell3 = newRow.insertCell(2);
     let cell4 = newRow.insertCell(3);
     let cell5 = newRow.insertCell(4);
-    // var btn11=document.createElement('button')
-    cell1.textContent = data.fname;
-    cell2.textContent = data.roll;
-    cell3.textContent = data.mobile;
+   
+    cell1.textContent = fullName;
+    cell2.textContent = roll_number;
+    cell3.textContent = mobile;
     let btn11=document.createElement('button');
-
-    cell5.appendChild(btn11);
-    btn11.id='editpro'
+    btn11.id=`editpro(${index})`
     btn11.textContent='Edit'
-    btn11.setAttribute("onClick","edit(this)")
-
+    btn11.setAttribute("onClick",`javascript:edit(${index})`)
+    btn11.style.backgroundColor="#03fc3d"
+    btn11.style.borderRadius='5px'
+    btn11.style.color="#040505"
+    cell5.appendChild(btn11);
+   
     let btn112=document.createElement('button');
-    cell5.appendChild(btn112);
-    btn112.id='dlt'
+    btn112.id=`dlt(${index})`
     btn112.textContent='Delete'
-    btn112.setAttribute("onClick","Delete(this)")
-}
-
+    btn112.style.backgroundColor="#f20000"
+    btn112.style.borderRadius='5px'
+    btn112.style.color="#040505"
+    btn112.setAttribute("onClick",`javascript:Delete(${index})`)
+    cell5.appendChild(btn112); 
+} 
 // .....................................................
 
-//Delete the data
-function Delete(td)
+/* when we click on edit button the data will be display on the form and after clicking
+on save button the data will be save on the same row and also update on localstorage */
+function edit(index) 
 {
-    // let rowData=td.parentElement.parentElement;
-    // console.log(rowData.children[1]);
+  var stdObj=stdArray[index];
+selectedindex=index;
+    document.getElementById("fullName").value = stdObj.fullName;
+    document.getElementById("roll_number").value = stdObj.roll_number;
+    document.getElementById("mobile").value = stdObj.mobile;
+  document.getElementById('btn').textContent="save";
+  
+}
+// ...............................
+
+//this function will delete the data from the localstorage and also from the browser
+function Delete(index)
+{
+   
     if (confirm('Do you want to delete this record?')) 
     {
-      let rowData= document.getElementById('tabody')
-        rowData.deleteRow(td);
+      stdArray.splice(index, 1);
+      localStorage.StudentJson= JSON.stringify(stdArray);
+       
+         init();
+      
      
-         resetForm();
       }
+
+      
 }
 // ...............................................
 
-//Edit the data
-function edit(td) 
+// clear function will clear the inputs which is giving in the form
+function clear()
 {
-  // console.log("msg....")
-    selectedRow = td.parentElement.parentElement;
-    document.getElementById("fname").value = selectedRow.cells[0].textContent;
-    document.getElementById("roll").value = selectedRow.cells[1].textContent;
-    document.getElementById("mobile").value = selectedRow.cells[2].textContent;
-    
+  selectedindex= -1;
+  document.getElementById("fullName").value="";
+  document.getElementById("roll_number").value="";
+  document.getElementById("mobile").value ="";
+  document.getElementById('btn').textContent="Submit";
+
 }
 // .............................................
 
-// update
-function updateRecord(formData) 
-{
-    selectedRow.cells[0].textContent = formData.fname;
-    selectedRow.cells[1].textContent = formData.roll;
-    selectedRow.cells[2].textContent = formData.mobile;
-    
-}
-// ............................................
-
-//Reset form
-function resetForm() 
-{
-    document.getElementById("fname").value = '';
-    document.getElementById("roll").value = '';
-    document.getElementById("mobile").value = '';
-    
-    selectedRow = null;
-}
-// ....................................
-
-function readFormData() 
-{
-    var formData = {};
-    formData["fname"] = document.getElementById("fname").value;
-    formData["roll"] = document.getElementById("roll").value;
-    formData["mobile"] = document.getElementById("mobile").value;
-    
-    return formData;
-}
-// ............................
 // design for student form
 var body=document.querySelector('body');
 body.style.backgroundImage="url('back.png')";
@@ -249,6 +263,7 @@ form.style.border="1px solid rgba(255,255,255,0.3)";
 form.style.background="rgba(255,255,255,0.2)"
 form.style.borderRadius="15px"
 form.style.boxShadow="0 4px 30px rgba(0,0,0,0.1)"
+
 var b= document.querySelector('button')
 b.style.color="#fff"
 b.style.fontSize="15px"
@@ -256,12 +271,30 @@ b.style.border="1px solid rgba(255,255,255,0.3)"
 b.style.borderRadius="5px"
 b.style.margin="5px 0 10px"
 b.style.backgroundColor="#3B5999"
-// design for data show and delete
+
+let button11=document.getElementById('button1')
+button11.style.color="#fff"
+button11.style.fontSize="15px"
+button11.style.border="1px solid rgba(255,255,255,0.3)"
+button11.style.borderRadius="5px"
+button11.style.margin="5px 0 10px"
+button11.style.backgroundColor="#3B5999"
+
 var trow=document.getElementById('list')
 trow.style.backgroundColor="#3B5999"
 trow.style.color="#ffffff"
 trow.style.minWidth="500px"
-trow.style.padding="5px"
-var tab=document.getElementById("tabody");
-tab.style.color="black";
+trow.style.padding="8px"
+trow.style.border="1px solid white"
+trow.style.fontFamily="Lucida Console"
+var tab=document.getElementById("tableRow");
+tab.style.color="#03fcdf";
 tab.style.fontSize="18px"
+tab.style.fontFamily=" serif"
+button121.style.color="#fff"
+button121.style.fontSize="15px"
+button121.style.border="1px solid rgba(255,255,255,0.3)"
+button121.style.borderRadius="5px"
+button121.style.margin="5px 0 10px"
+button121.style.backgroundColor="#3B5999"
+
